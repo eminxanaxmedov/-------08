be5d159ac059e73a2791699822f3f8448f969125
eminxanaxmedov <xanaxmedov15@gmail.com>
2024-10-09 20:35:46 +0400

    vvv

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// Определяем объект computer
+
 let computer = {
     name: "Asus",
     model: "Rog Strix",
-    date: "2023-01-15", // допустим, добавим дату
+    date: "2023-01-15", 
     capacity: "1024GB"
 };
 
-// Определяем объект phone
+
 let phone = {
     name: "Samsung",
     model: "Galaxy S21",
-    date: "2021-02-11", // также добавим дату
+    date: "2021-02-11", 
     capacity: "256GB"
 };
 
-// Создаем основной объект good
 let good = {
-    // Метод fullInfo возвращает информацию из объектов computer и phone
+
     fullInfo: function() {
         return `Компьютер: ${this.computer.name} ${this.computer.model}, 
         Дата: ${this.computer.date}, 
@@ -25,10 +24,10 @@ let good = {
         Дата: ${this.phone.date}, 
         Вместимость: ${this.phone.capacity}`;
     },
-    // Связываем объекты computer и phone с good
+   
     computer: computer,
     phone: phone
 };
 
-// Выводим полную информацию в консоль
+
 console.log(good.fullInfo());
